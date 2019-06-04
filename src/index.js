35a5131db77b0768255ecbca6a2223a684b5bccc
Emil Haugberg van Veen <emilveen@gmail.com>
2019-06-04 16:47:45 +0200

    Add skeleton libary. #8

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
+import css1 from "./css/normalize.css";
+import css2 from "./css/skeleton.css";
 import React from "react";
 import ReactDOM from "react-dom";
 
 const App = () => {
-  return <div>Hello Poeperd!</div>;
+  return <div>Hello Object!</div>;
 };
 
 ReactDOM.render(<App />, document.querySelector("#root"));
