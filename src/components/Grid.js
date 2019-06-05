1b49e3fbb7cbca2e0c3502e3cfd637e61479a77d
ralph <erkamps12@gmail.com>
2019-06-05 13:58:29 +0200

    added dataContainer class to show data in

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,12 +1,14 @@
 import chota from "chota";
 import React from 'react';
+import DataContainer from './DataContainer'
+// import css from ""
 
 class Grid extends React.Component {
     render() {
         return (
             <div class="grid">
                 <div class="row">
-                    <div class="col-4"> .col-4 </div>
+                    <div class="col-4"> <DataContainer/> </div>
                     <div class="col-4"> .col-4 </div>
                     <div class="col-4"> .col-4 </div>
                 </div>
