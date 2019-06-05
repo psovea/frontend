7d2c917e260ddf2882c40fcc4886684b2fb6bc24
Eric van der Woude <ericvdwoude@gmail.com>
2019-06-05 14:55:51 +0200

    resolved merge conflict, again... #9

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
+/* eslint no-unused-vars: 0 */
+import chota from "chota";
 import React from "react";
 import ReactDOM from "react-dom";
 import Application from "./components/Application/Application.js"
 
 ReactDOM.render(<Application />, document.getElementById('root'));
+
