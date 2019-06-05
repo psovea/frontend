c37a43bdac62f5e6f0e237b914f3b292999c6cc1
Eric van der Woude <ericvdwoude@gmail.com>
2019-06-05 15:45:01 +0200

    Tweaked linter so it uses recommendations by React self

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
         "browser": true,
         "es6": true
     },
-    "extends": "eslint:recommended",
+    "extends": "plugin:react/recommended",
     "globals": {
         "Atomics": "readonly",
         "SharedArrayBuffer": "readonly"
