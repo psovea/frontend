2892d78e5e9bb598a399d3907159037707a6c2a5
Rick Teuthof <rmt1998@hotmail.nl>
2019-06-05 21:25:10 +0200

    Trying to get React Leaflet to work #25

--- src/components/Application/Application.js
+++ src/components/Application/Application.js
@@ -3,18 +3,19 @@ import React from 'react';
 import Grid from '../Grid/Grid.js';
 import Sidebar from '../Sidebar/Sidebar.js';
 import './Application.css';
+import Maps from '../Maps/Maps'
 
 class Application extends React.Component {
     render() {
         return (
-        <div id="main" class="row">
-            <div class="col-2">
-                <Sidebar />
+            <div id="main" className="row">
+                <div className="col-2">
+                    <Sidebar />
+                </div>
+                <div className="col-10">
+                    <Grid />
+                </div>
             </div>
-            <div class="col-10">
-                <Grid />
-            </div>
-        </div>
         );
     }
 }
