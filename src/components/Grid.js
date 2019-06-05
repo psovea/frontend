785da140c578eb62ad4de8abae5c7cd5865790a8
Rick Teuthof <rmt1998@hotmail.nl>
2019-06-05 13:42:30 +0200

    Added chart example #15

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,29 +1,32 @@
 import chota from "chota";
 import React from 'react';
+import Graph from './Graph'
+
+
 
 class Grid extends React.Component {
     render() {
         return (
             <div>
-                <div class="test">
+                <div>
                     <h3>This is a heading in a div element</h3>
                     <p>This is some text in a div element.</p>
                 </div>
                 <div>
-                    <div class="row">
-                        <div class="col-4"> .col-4 </div>
-                        <div class="col-4"> .col-4 </div>
-                        <div class="col-4"> .col-4 </div>
+                    <div className="row">
+                        <div className="col-4"> <Graph /> </div>
+                        <div className="col-4"> .col-4 </div>
+                        <div className="col-4"> .col-4 </div>
                     </div>
-                    <div class="row">
-                        <div class="col-4"> .col-4 </div>
-                        <div class="col-4"> .col-4 </div>
-                        <div class="col-4"> .col-4 </div>
+                    <div className="row">
+                        <div className="col-4"> .col-4 </div>
+                        <div className="col-4"> .col-4 </div>
+                        <div className="col-4"> .col-4 </div>
                     </div>
-                    <div class="row">
-                        <div class="col-4"> .col-4 </div>
-                        <div class="col-4"> .col-4 </div>
-                        <div class="col-4"> .col-4 </div>
+                    <div className="row">
+                        <div className="col-4"> .col-4 </div>
+                        <div className="col-4"> .col-4 </div>
+                        <div className="col-4"> .col-4 </div>
                     </div>
                 </div>
             </div>
