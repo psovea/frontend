16dd3d9d1f4696518277e7f001664824a95dff77
ralph <erkamps12@gmail.com>
2019-06-05 12:57:01 +0200

    issue/9 - grid height and other css properties are being added

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -4,27 +4,21 @@ import React from 'react';
 class Grid extends React.Component {
     render() {
         return (
-            <div>
-                <div class="test">
-                    <h3>This is a heading in a div element</h3>
-                    <p>This is some text in a div element.</p>
+            <div class="grid">
+                <div class="row">
+                    <div class="col-4"> .col-4 </div>
+                    <div class="col-4"> .col-4 </div>
+                    <div class="col-4"> .col-4 </div>
                 </div>
-                <div>
-                    <div class="row">
-                        <div class="col-4"> .col-4 </div>
-                        <div class="col-4"> .col-4 </div>
-                        <div class="col-4"> .col-4 </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-4"> .col-4 </div>
-                        <div class="col-4"> .col-4 </div>
-                        <div class="col-4"> .col-4 </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-4"> .col-4 </div>
-                        <div class="col-4"> .col-4 </div>
-                        <div class="col-4"> .col-4 </div>
-                    </div>
+                <div class="row">
+                    <div class="col-4"> .col-4 </div>
+                    <div class="col-4"> .col-4 </div>
+                    <div class="col-4"> .col-4 </div>
+                </div>
+                <div class="row">
+                    <div class="col-4"> .col-4 </div>
+                    <div class="col-4"> .col-4 </div>
+                    <div class="col-4"> .col-4 </div>
                 </div>
             </div>
         );
