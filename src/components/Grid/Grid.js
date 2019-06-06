57bcc32de8de3c940ba5598e062b6f84b2be91e3
ralph <erkamps12@gmail.com>
2019-06-06 14:21:44 +0200

    replaced template piechart with the doughnut component

--- src/components/Grid/Grid.js
+++ src/components/Grid/Grid.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Responsive, WidthProvider } from 'react-grid-layout';
 import Graph from '../Graph/Graph.js';
+import Maps from '../Maps/Maps.js'
+import DoughnutChart from '../DoughnutChart/DoughnutChart.js'
 import './Grid.css';
-import Maps from '../Maps/Maps'
+
 // We need these css imports, else the graphics will glitch
 // while moving components in our grid.
 import '../../../node_modules/react-grid-layout/css/styles.css';
@@ -21,12 +23,12 @@ class Grid extends React.Component {
                 breakpoints={{ lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0 }}
                 cols={{ lg: 3, md: 3, sm: 2, xs: 2, xxs: 2 }}
             >
-                <div key="pie" className="pie" data-grid={{ x: 0, y: 2, w: 1, h: 1, maxW: 1 }}>Amazing pie chart</div>
                 <div key="stats" className="stats" data-grid={{ x: 1, y: 0, w: 2, h: 1 }}>Other amazing stats</div>
                 <div key="chart" data-grid={{ x: 0, y: 3, w: 3, h: 2 }}>
                     <Graph />
                 </div>
-                <div key="map" data-grid={{ x: 0, y: 3, w: 3, h: 3, static: true }} >
+                <div key="Donut" data-grid={{ x: 0, y: 2, w: 1, h: 2 }}> <DoughnutChart /> </div>
+                <div key="map" data-grid={{ x: 0, y: 2, w: 3, h: 3, static: true }} >
                     <Maps />
                 </div>
             </ResponsiveGridLayout>
