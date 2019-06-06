7eec24b5a868b3b63e5babeb726956a9cf35a3a4
Sebastiaan van Moergstel <sebastiaanvm@live.nl>
2019-06-06 16:09:02 +0200

    Top10 bottlenecks added #33

--- src/components/Grid/Grid.js
+++ src/components/Grid/Grid.js
@@ -22,14 +22,29 @@ class Grid extends React.Component {
                 cols={{ lg: 3, md: 3, sm: 2, xs: 2, xxs: 2 }}
             >
                 {/* x and y are the position of the block on the grid. w is the width and h the height of the block. */}
-                <div key="pie" className="pie" data-grid={{ x: 0, y: 0, w: 1, h: 1, maxW: 1 }}>Amazing pie chart</div>
-                <div key="stats" className="stats" data-grid={{ x: 1, y: 0, w: 2, h: 1 }}>Other amazing stats</div>
-                <div key="chart" data-grid={{ x: 0, y: 1, w: 3, h: 2 }}>
-                    <Graph />
-                </div>
-                <div key="map" data-grid={{ x: 0, y: 3, w: 3, h: 5, static: true }} >
+                <div key="pie" className="pie" data-grid={{ x: 0, y: 0, w: 1, h: 2, maxW: 1 }}>Amazing pie chart</div>
+                <div key="map" data-grid={{ x: 2, y: 0, w: 2, h: 2, static: true }} >
                     <Maps />
                 </div>
+                
+                <div key="stats" className="stats" data-grid={{ x: 0, y: 2, w: 2, h: 3 }}>Other amazing stats</div>
+                <div key="bottle" className="stats" data-grid={{ x: 2, y: 2, w: 1, h: 3 }}>
+                    <h1>Top 10 bottlenecks:</h1>
+                    1. <br></br>
+                    2. <br></br>
+                    3. <br></br>
+                    4. <br></br>
+                    5. <br></br>
+                    6. <br></br>
+                    7. <br></br>
+                    8. <br></br>
+                    9. <br></br>
+                    10. <br></br>
+                </div>
+                <div key="chart" data-grid={{ x: 0, y: 5, w: 3, h: 2 }}>
+                    <Graph />
+                </div>
+                
             </ResponsiveGridLayout>
         )
     }
