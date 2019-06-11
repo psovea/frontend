3bb8ce5c58e7727c8abab1759719db411653cd85
Rick Teuthof <rmt1998@hotmail.nl>
2019-06-11 12:36:25 +0200

    Added geoJSON functionality #25

--- src/components/Maps/Maps.js
+++ src/components/Maps/Maps.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Map, TileLayer, Marker, Popup } from 'react-leaflet'
+import { Map, TileLayer, Marker, Popup, GeoJSON } from 'react-leaflet'
 import HeatmapLayer from 'react-leaflet-heatmap-layer'
 import { addressPoints } from './DummyHeatmap'
 
@@ -9,16 +9,17 @@ class Maps extends React.Component {
         this.state = {
             bounds: [[52.462449, 4.738163], [52.290331, 5.135141]],
             center: [52.3680, 4.9036],
-            initZoom: 13,
-            data: []
+            zoom: 13,
+            stops: [],
+            districts: []
         }
     };
 
     createMarkers() {
         let markers = []
-        let data = this.state.data
-        for (let i = 0; i < data.length; i++) {
-            let element = data[i][Object.keys(data[i])[0]]
+        let stops = this.state.stops
+        for (let i = 0; i < stops.length; i++) {
+            let element = stops[i][Object.keys(stops[i])[0]]
             let position = [element.lat, element.lon]
             markers.push(
                 <Marker
@@ -34,21 +35,25 @@ class Maps extends React.Component {
     componentDidMount() {
         fetch(`https://cors-anywhere.herokuapp.com/http://184.72.120.43:9800/getStops`)
             .then(res => res.json())
-            .then(json => this.setState({ data: json }));
+            .then(json => this.setState({ stops: json }))
+        fetch(`https://cors-anywhere.herokuapp.com/http://145.109.9.102:5000/get-districts`)
+            .then(res => res.json())
+            .then(json => this.setState({ districts: json }))
     }
 
     render() {
+        console.log(this.state.stops)
+        console.log(this.state.districts)
         return (
             <Map
                 ref={(ref) => { this.map = ref; }}
                 center={this.state.center}
-                zoom={this.state.initZoom}
+                zoom={this.state.zoom}
                 bounds={this.state.bounds}
                 maxBounds={this.state.bounds}
                 boundsOptions={{ padding: [50, 50] }}
                 maxZoom={15}
                 minZoom={11}
-            // onzoomend={(e) => { console.log(e); }}
             >
                 <HeatmapLayer
                     fitBoundsOnLoad
@@ -63,7 +68,8 @@ class Maps extends React.Component {
                     url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
                 />
                 {this.createMarkers()}
-            </Map >
+                <GeoJSON data={this.state.districts} />
+                </Map >
         );
     }
 }
