5069c913a251c60df92d97228b27c304ae1c233c
ralph <erkamps12@gmail.com>
2019-06-13 20:51:41 +0200

    reordered grid layout

--- src/components/Grid/Grid.js
+++ src/components/Grid/Grid.js
@@ -31,20 +31,20 @@ class Grid extends React.Component {
       field1Value: true,
       field2Value: true,
       field3Value: true,
-      field4Value: true
+    //   field4Value: true
     }
 
     this.shouldData1Render = this.shouldData1Render.bind(this)
     this.shouldData2Render = this.shouldData2Render.bind(this)
     this.shouldData3Render = this.shouldData3Render.bind(this)
-    this.shouldData4Render = this.shouldData4Render.bind(this)
+    // this.shouldData4Render = this.shouldData4Render.bind(this)
   }
 
   shouldComponentUpdate (nextProps, nextState) {
     nextState.field1Value = nextProps.field1Value
     nextState.field2Value = nextProps.field2Value
     nextState.field3Value = nextProps.field3Value
-    nextState.field4Value = nextProps.field4Value
+    // nextState.field4Value = nextProps.field4Value
     return true
   }
 
@@ -64,7 +64,8 @@ class Grid extends React.Component {
     if (this.state.field2Value === true) {
       shouldRender =
         <div key='2' className='pie' data-grid={{ x: 0, y: 0, w: 2, h: 4 }}>
-          <DoughnutChart />
+          {/* <DoughnutChart /> */}
+          <Feed kind='delay' />
         </div>
     }
     return shouldRender
@@ -74,23 +75,23 @@ class Grid extends React.Component {
     var shouldRender = <div />
     if (this.state.field3Value === true) {
       shouldRender =
-        <div key='3' className='pie' data-grid={{ x: 0, y: 4, w: 2, h: 3 }}>
+        <div key='3' className='pie' data-grid={{ x: 0, y: 4, w: 4, h: 3 }}>
           <BarChart />
         </div>
     }
     return shouldRender
   }
 
-  shouldData4Render () {
-    var shouldRender = <div />
-    if (this.state.field4Value === true) {
-      shouldRender =
-        <div key='4' className='pie' data-grid={{ x: 2, y: 4, w: 2, h: 3 }}>
-          <Feed kind='delay' />
-        </div>
-    }
-    return shouldRender
-  }
+//   shouldData4Render () {
+//     var shouldRender = <div />
+//     if (this.state.field4Value === true) {
+//       shouldRender =
+//         <div key='4' className='pie' data-grid={{ x: 2, y: 4, w: 2, h: 3 }}>
+//           <Feed kind='delay' />
+//         </div>
+//     }
+//     return shouldRender
+//   }
 
   render () {
     console.log(this.field1Value)
@@ -103,7 +104,7 @@ class Grid extends React.Component {
         {this.shouldData1Render()}
         {this.shouldData2Render()}
         {this.shouldData3Render()}
-        {this.shouldData4Render()}
+        {/* {this.shouldData4Render()} */}
       </ResponsiveGridLayout>
     )
   }
@@ -113,7 +114,7 @@ Grid.propTypes = {
   field1Value: PropTypes.bool,
   field2Value: PropTypes.bool,
   field3Value: PropTypes.bool,
-  field4Value: PropTypes.bool
+//   field4Value: PropTypes.bool
 }
 
 const mapStateToProps = state => {
