09f9deb11d3bdacb2497a079d7b8db9dc25fb4b8
Florine de Geus <florine.degeus@student.uva.nl>
2019-06-13 18:32:37 +0200

    Added live feed for delays. TODO: Connect to webSocket #46

--- src/components/Feed/Delay.js
+++ src/components/Feed/Delay.js
@@ -1,34 +1,83 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { SubscribeToMessage } from '../../api/Socket';
 
 class Delay extends React.Component {
   constructor(props) {
     super(props);
 
-    SubscribeToMessage((err, message) => this.SetState({
-      message
-    }))
-
-    state = {
-      message: 'None'
+    this.state = {
+      publicLine: '0',
+      name: '-',
+      transportType: '-',
+      operator: '-',
+      stopName: '-',
+      punctuality: 0,
+      timestamp: 0
     }
   }
 
-  render = () => {
-    // var delay = this.props.delay;
+  getTravelInfo(info) {
+    var newState = {
+      publicLine: '0',
+      name: '-',
+      transportType: '-',
+      operator: info.dataownercode,
+      stopName: '-',
+      punctuality: info.punctuality,
+      timestamp: info.timestamp
+    }
 
-    console.log(this.state.message);
+    fetch("https://cors-anywhere.herokuapp.com/" + `http://18.216.203.6:5000/get-stops?stop_code=300${info.userstopcode}`)
+    .then(res => res.json())
+    .then(res => {
+      console.log(res[0].name)
+      this.setState({stopName: res[0].name});
+    })
+
+    fetch("https://cors-anywhere.herokuapp.com/" + `http://18.216.203.6:5000/get-lines?operator=${info.dataownercode}&internal_id=${info.lineplanningnumber}`)
+    .then(res => res.json())
+    .then(res => {
+      this.setState({
+        publicLine: res[0].public_id,
+        transportType: res[0].transport_type,
+        name: res[0].name,
+        operator: res[0].operator
+      })
+    })
+
+    console.log(newState);
+  }
+
+  componentDidMount() {
+    var info = {
+      dataownercode: 'GVB',
+      lineplanningnumber: '66',
+      operatingday: '2019-06-13',
+      journeynumber: '303',
+      reinforcementnumber: '0',
+      userstopcode: '08299',
+      passagesequencenumber: '0',
+      timestamp: '2019-06-13T17:21:16.4937073+02:00',
+      source: 'SERVER',
+      vehiclenumber: '1130',
+      punctuality: '96'
+    }
+
+    this.getTravelInfo(info);
+  }
+
+  render = () => {
+    console.log(this.state);
     return (
       <li className={"delay"}>
-        <p>Message: {this.state.timestamp}</p>
+        <p className={"delay-header"}>Lijn {this.state.publicLine}: {this.state.stopName}</p>
+        <p>Halte: {this.state.stopName}</p>
+        <p>Transport type: {this.state.transportType}</p>
+        <p>Vervoerder: {this.state.operator}</p>
+        <p>Vertraging: {this.state.punctuality} seconden</p>
+
       </li>
     )
   }
 }
-
-// Delay.propTypes = {
-//   delay: PropTypes.number
-// }
 
 export default Delay;
--- src/components/Feed/Delays.js
+++ src/components/Feed/Delays.js
@@ -8,18 +8,6 @@ import './Feed.css'
 class Delays extends React.Component {
 
   render = () => {
-    // var content = this.props.delays.map((delay) => {
-    //   return (
-    //     <Delay key={delay.id} delay={delay} />
-    //   )
-    // })
-
-    // console.log("wat");
-
-
-    // return (
-    //   <ul className="delays">Content hier: {content}</ul>
-    // )
 
     return (
       <ul className="delays">
