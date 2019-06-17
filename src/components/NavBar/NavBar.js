fde1e45c2c04999c70c5be8621d58b38f4a450c2
Florine de Geus <florine.degeus@student.uva.nl>
2019-06-17 17:33:00 +0200

    Included header in logo

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import logo from '../../assets/logo_red.svg';
+import andreas from '../../assets/andreaskruisen.svg'
 
 import './NavBar.css';
 
 class NavBar extends Component {
-    
+
     constructor(props) {
         super(props);
     }
@@ -11,7 +13,14 @@ class NavBar extends Component {
     render() {
         return (
             <div className="navbar">
-
+                <div className="logoleft">
+                    <img src={logo}/>
+                    <span>PSOVEA</span>
+                </div>
+                <div className="logoright">
+                    <span>Amsterdam</span>
+                    <img src={andreas}/>
+                </div>
             </div>
         )
     }
