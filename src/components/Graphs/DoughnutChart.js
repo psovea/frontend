01cac4a3ed0349407333c6089480ea1b1e58af29
Rick Teuthof <rmt1998@hotmail.nl>
2019-06-18 10:58:56 +0200

    Added setup for Settings #61

--- src/components/Graphs/DoughnutChart.js
+++ src/components/Graphs/DoughnutChart.js
@@ -1,76 +1,71 @@
 import React from 'react';
 import { Doughnut } from 'react-chartjs-2';
-import Tooltip from 'rc-tooltip';
 import Slider from 'rc-slider';
 import 'rc-slider/assets/index.css';
 import './Graphs.css';
+import Settings from '../Settings/Settings'
 
 const data = {
-	labels: [
-		'Red',
-		'Green',
-		'Yellow'
-	],
-	datasets: [{
-		data: [300, 50, 100],
-		backgroundColor: [
-		'#FF6384',
-		'#36A2EB',
-		'#FFCE56'
-		],
-		hoverBackgroundColor: [
-		'#FF6384',
-		'#36A2EB',
-		'#FFCE56'
-		]
-	}]
+    labels: [
+        'Red',
+        'Green',
+        'Yellow'
+    ],
+    datasets: [{
+        data: [300, 50, 100],
+        backgroundColor: [
+            '#FF6384',
+            '#36A2EB',
+            '#FFCE56'
+        ],
+        hoverBackgroundColor: [
+            '#FF6384',
+            '#36A2EB',
+            '#FFCE56'
+        ]
+    }]
 };
 
-const content = <div className="dashboard-widget-content" id="bar">
-                    <Doughnut data={data} options={{ responsive:true, maintainAspectRatio: false }}/>
-                </div>;
+// const settings = (state) => <div className={"dashboard-widget-content-settings " + (state.settings ? "show" : "hide")} id="bar">
+//     <div className="dashboard-widget-content-settings-container">
+//         <div className="dashboard-widget-content-settings-container-content">
+//             <p className="dashboard-widget-content-settings-container-content-title">Slider</p>
+//             <hr />
+//             <Slider min={20} defaultValue={20} marks={{ 20: "1 dag", 40: "3 dagen", 60: "1 week", 100: "2 weken" }} step={null} />
+//         </div>
+//         <div className="dashboard-widget-content-settings-container-content">
+//             <p className="dashboard-widget-content-settings-container-content-title">Vervoerstype</p>
+//             <hr />
 
-const settings = (state) => <div className={"dashboard-widget-content-settings " + (state.settings ? "show" : "hide")} id="bar">
-                    <div className="dashboard-widget-content-settings-container">
-                        <div className="dashboard-widget-content-settings-container-content">
-                            <p className="dashboard-widget-content-settings-container-content-title">Slider</p>
-                            <hr/>
-                            <Slider min={20} defaultValue={20} marks={{ 20: "1 dag", 40: "3 dagen", 60: "1 week", 100: "2 weken" }} step={null} />
-                        </div>
-                        <div className="dashboard-widget-content-settings-container-content">
-                            <p className="dashboard-widget-content-settings-container-content-title">Vervoerstype</p>
-                            <hr/>
+//         </div>
+//         <div className="dashboard-widget-content-settings-container-content">
+//             <p className="dashboard-widget-content-settings-container-content-title">Slider</p>
+//             <hr />
+//         </div>
+//         <div className="dashboard-widget-content-settings-container-content">
+//             <p className="dashboard-widget-content-settings-container-content-title">Vervoerstype</p>
+//             <hr />
 
-                        </div>
-                        <div className="dashboard-widget-content-settings-container-content">
-                            <p className="dashboard-widget-content-settings-container-content-title">Slider</p>
-                            <hr/>
-                        </div>
-                        <div className="dashboard-widget-content-settings-container-content">
-                            <p className="dashboard-widget-content-settings-container-content-title">Vervoerstype</p>
-                            <hr/>
-
-                        </div>
-                        <div className="dashboard-widget-content-settings-container-content">
-                            <p className="dashboard-widget-content-settings-container-content-title">Slider</p>
-                            <hr/>
-                        </div>
-                        <div className="dashboard-widget-content-settings-container-content">
-                            <p className="dashboard-widget-content-settings-container-content-title">Vervoerstype</p>
-                            <hr/>
-                        </div>
-                    </div>
-                    <div className="dashboard-widget-content-settings-buttons">
-                        <div className="dashboard-widget-content-settings-buttons-container">
-                            <button className="dashboard-widget-content-settings-buttons-button button outline primary"><i className="dashboard-widget-settings-button-icon fa fa-check"></i> apply</button>
-                            <button className="dashboard-widget-content-settings-buttons-button button outline secondary"><i className="dashboard-widget-settings-button-icon fa fa-refresh"></i> reset</button>
-                        </div>
-                    </div>
-                </div>
+//         </div>
+//         <div className="dashboard-widget-content-settings-container-content">
+//             <p className="dashboard-widget-content-settings-container-content-title">Slider</p>
+//             <hr />
+//         </div>
+//         <div className="dashboard-widget-content-settings-container-content">
+//             <p className="dashboard-widget-content-settings-container-content-title">Vervoerstype</p>
+//             <hr />
+//         </div>
+//     </div>
+//     <div className="dashboard-widget-content-settings-buttons">
+//         <div className="dashboard-widget-content-settings-buttons-container">
+//             <button className="dashboard-widget-content-settings-buttons-button button outline primary"><i className="dashboard-widget-settings-button-icon fa fa-check"></i> apply</button>
+//             <button className="dashboard-widget-content-settings-buttons-button button outline secondary"><i className="dashboard-widget-settings-button-icon fa fa-refresh"></i> reset</button>
+//         </div>
+//     </div>
+// </div>
 
 class DoughnutChart extends React.Component {
-
-	constructor(props) {
+    constructor(props) {
         super(props);
         this.state = {
             settings: false,
@@ -80,12 +75,16 @@ class DoughnutChart extends React.Component {
     }
 
     settingsClicked() {
-        this.setState({settings: !this.state.settings})
+        this.setState({ settings: !this.state.settings })
     }
 
-	render() {
-        const settingsIcon = <i className="dashboard-widget-header-settings-wrapper-icon fa fa-sliders" aria-hidden="true"/>
-        const backIcon = <i className="dashboard-widget-header-settings-wrapper-icon fa fa-reply" aria-hidden="true"/>
+    settingObject = {
+        slider: <Slider min={20} defaultValue={20} marks={{ 20: "1 dag", 40: "3 dagen", 60: "1 week", 100: "2 weken" }} step={null} />
+    }
+
+    render() {
+        const settingsIcon = <i className="dashboard-widget-header-settings-wrapper-icon fa fa-sliders" aria-hidden="true" />
+        const backIcon = <i className="dashboard-widget-header-settings-wrapper-icon fa fa-reply" aria-hidden="true" />
 
         return (
             <div className="dashboard-widget">
@@ -98,11 +97,13 @@ class DoughnutChart extends React.Component {
                         {settingsIcon}
                     </div>
                 </div>
-                {content}
-                {settings(this.state)}
+                <div className="dashboard-widget-content" id="bar">
+                    <Doughnut data={data} options={{ responsive: true, maintainAspectRatio: false }} />
+                </div>;
+                <Settings atributes={this.settingObject} />
             </div>
         );
-	}
+    }
 }
 
 export default DoughnutChart; 
