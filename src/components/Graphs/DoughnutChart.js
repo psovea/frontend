24bb86efd811da201c702ef946fbb142b821b480
ralph <erkamps12@gmail.com>
2019-06-19 13:55:44 +0200

    setup done

--- src/components/Graphs/DoughnutChart.js
+++ src/components/Graphs/DoughnutChart.js
@@ -29,6 +29,45 @@ const data = {
     }]
 };
 
-const DoughnutChart = () => <Doughnut data={data} options={{ responsive: true, maintainAspectRatio: false }} />
+class DoughnutChart extends React.Component {
+
+    constructor(props) {
+        super(props);
+        console.log("Constructor")
+        this.state = {
+        }
+    }
+
+    fetchJSON() {
+        console.log("Fetching")
+        // url = 'http://18.224.29.151:5000/get-district-delays'
+        // let jsonVar = {}
+        // fetch(url, {
+        //     headers: {
+        //         'Content-Type': 'application/json',
+        //         'Accept': 'application/json'
+        //     }
+        // }).then(res => {
+        //     // return res.json();
+        //     console.log(res.json());
+        // }).then(json => {
+        //     console.log(json)
+        //     // jsonVar[value] = json;
+        //     // this.setState(jsonVar);
+        // })
+    }
+
+    render() {
+        console.log("render")
+        return (
+            <Doughnut data={data} options={{ responsive: true, maintainAspectRatio: false }} />
+        );
+    }
+    
+    componentDidMount() {
+        console.log("ccomponentDidMount")
+        this.fetchJSON()
+    }
+}
 
 export default DoughnutChart; 
