cf6cfa5c4902dd5168364a21efdfbecbae3f17b5
Sebastiaan van Moergstel <sebastiaanvm@live.nl>
2019-06-20 14:44:55 +0200

    deleted code from emils suggestion

--- src/components/Graphs/BarChart.js
+++ src/components/Graphs/BarChart.js
@@ -7,20 +7,6 @@ import React from 'react';
 import { Bar } from 'react-chartjs-2';
 import './Graphs.css';
 
-// const data = {
-//     labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-//     datasets: [
-//         {
-//             label: 'Vertraging',
-//             backgroundColor: 'rgba(255,99,132,0.2)',
-//             borderColor: 'rgba(255,99,132,1)',
-//             borderWidth: 1,
-//             hoverBackgroundColor: 'rgba(255,99,132,0.4)',
-//             hoverBorderColor: 'rgba(255,99,132,1)',
-//             data: [65, 59, 80, 81, 56, 55, 40]
-//         }
-//     ]
-// };
 
 class BarChart extends React.Component {
     constructor(props) {
