09cec056d35819ebacf2b4eb6737b3aabeedcaa7
Florine <22481916+enirolf@users.noreply.github.com>
2019-06-24 21:03:41 +0200

    Reformatted tables and added filter options #139 (#141)

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -5,7 +5,9 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Missing from '../Missing/Missing'
-import ReactTable from 'react-table'
+// import ReactTable from 'react-table'
+
+import * as R from 'ramda'
 
 
 import "./Table.css"
@@ -16,13 +18,35 @@ class DataTable extends React.Component {
         super(props);
 
         this.state = {
-            headers: this.props.headers,
+            headers: [],
             values: []
         }
     }
 
+    componentDidMount() {
+        this.setState({ headers: this.props.headers })
+    }
+
     update(newData) {
-        this.setState({values: newData[0]})
+        /* If the new data contains stop data, we should fetch the
+         * stop names to display.
+         */
+        const hasStops = R.hasPath(['metric', 'stop_end'])
+        if (R.all(hasStops)(newData)) {
+            this.getStopNames(newData)
+                .then(res => R.zipWith((x, y) => x.metric.stop_end = y, newData, res))
+                .then(res => this.setState({values: newData}))
+        } else {
+            this.setState({values: newData})
+        }
+    }
+
+    getStopNames(data) {
+        var stops = R.join(",", R.map(item => item.metric.stop_end, data))
+
+        return fetch(`https://cors-anywhere.herokuapp.com/http://18.224.29.151:5000/get-stops?stop_code=${stops}`)
+            .then(res => res.json())
+            .then(res => R.map(stop => stop.stop_name, res))
     }
 
     formatTime(item) {
@@ -50,10 +74,10 @@ class DataTable extends React.Component {
                             let values = row.value
 
                             return <tr className="table-row" key={i}>
-                                <td className="table-row-value" key={i}>{i}</td>
+                                <td className="table-row-value" key={i}>{i + 1}</td>
                                 {
 
-                                    Object.keys(metric).map(val => {
+                                    this.props.order.map(val => {
                                         return <td className="table-row-value" key={metric[val]}>{metric[val]}</td>
                                     })
                                 }
@@ -73,7 +97,8 @@ DataTable.propTypes = {
     headers: PropTypes.any,
     values: PropTypes.any,
     top: PropTypes.any,
-    stateF: PropTypes.any
+    stateF: PropTypes.any,
+    order: PropTypes.any
 }
 
-export default DataTable;
+export default DataTable
