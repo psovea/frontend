b44b98c1b2d68567e804c9039c37ab6d8be0ff4f
Sebastiaan van Moergstel <sebastiaanvm@live.nl>
2019-06-26 11:22:32 +0200

    barchart now starts at 0

--- src/components/Graphs/BarChart.js
+++ src/components/Graphs/BarChart.js
@@ -37,12 +37,13 @@ class BarChart extends React.Component {
             labels: labelArray,
             datasets: [{
                 data: dataArray,
+
                 label: 'Vertraging (in uren)',
                 backgroundColor: 'rgba(255,99,132,0.2)',
                 borderColor: 'rgba(255,99,132,1)',
                 borderWidth: 1,
                 hoverBackgroundColor: 'rgba(255,99,132,0.4)',
-                hoverBorderColor: 'rgba(255,99,132,1)',
+                hoverBorderColor: 'rgba(255,99,132,1)'
             }]
         }
         return data
@@ -52,7 +53,7 @@ class BarChart extends React.Component {
         return (
             this.state.data.length == 0
                 ? <Missing/>
-                : <Bar data={this.makeData()} options={{ responsive: true, maintainAspectRatio: false }} />
+                : <Bar data={this.makeData()} options={{ responsive: true, maintainAspectRatio: false, scales: { yAxes: [{ ticks: { beginAtZero: true } }] } }} />
         )
     }
 }
